645bbb684edde66b31b21122c5b015d0ca21e515
Hartmut Bauer <hb42@gmx.de>
2019-04-08 22:14:41 +0200

    dev

---
+++ SbsdbServer/Model/Entities/UserSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hb.SbsdbServer.Model.Entities
+{
+    public partial class UserSettings
+    {
+        public decimal Id { get; set; }
+        public string Userid { get; set; }
+        public string Settings { get; set; }
+    }
+}
